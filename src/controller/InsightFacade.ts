25f3fbed18684ef47e88627bcde6e7113adc21f5
Rui Zhang <zhangrui_ca@sina.com>
2018-10-26 10:13:19 -0700

    clear

--- src/controller/InsightFacade.ts
+++ src/controller/InsightFacade.ts
@@ -10,7 +10,7 @@ import {Building} from "./Building";
 import {Decimal} from "decimal.js";
 import {Room} from "./Room";
 import {Section} from "./Section";
-import _ = require("lodash");
+
 /**
  * This is the main programmatic entry point for the project.
  * Method documentation is in IInsightFacade
